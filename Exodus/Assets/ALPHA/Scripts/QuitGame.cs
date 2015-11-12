13ddf3190d423dc588b5bc59075bdb030b6a4e2c
unknown <Dan.Schneider@web.de>
2015-11-12 22:52:15 +0100

    ADDED EndScreen Platzhalter; Close Game Script

---
+++ Exodus/Assets/ALPHA/Scripts/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour 
+{
+	void CloseGame()
+	{
+		if (Input.GetKey (KeyCode.Escape))
+		{
+			Application.Quit ();
+		}
+	}
+}

f18e6fff02e8be0891aba0dc0fb339eed82b56b8
unknown <Dan.Schneider@web.de>
2015-11-14 15:55:20 +0100

    FIXED Cleaned up Enemy Script

--- Exodus/Assets/ALPHA/Scripts/SeePlayer.cs
+++ Exodus/Assets/ALPHA/Scripts/SeePlayer.cs
@@ -3,91 +3,99 @@ using System.Collections;
 
 public class SeePlayer : MonoBehaviour 
 {
-	public float fieldOfViewAngle = 110f;           
-	public bool playerInSight;
-	private bool sisterInSight;
-	public GameObject sister;
-	public GameObject sisterHead;
-	private GameObject player;                      
-	private RaycastHit hit;
-	public GameObject eyes;
-	public float viewDistance;
-	public GameObject playerHead;
-	private Quaternion startRot;
-	public bool rot;
-	public Transform[] waypoints;
-	private int cur = 0;
-	public float speed = 0.3f;
+	[SerializeField] float FieldOfViewAngle = 110f;           
+	[SerializeField] bool PlayerInSight;
+	[SerializeField] GameObject Sister;
+	[SerializeField] GameObject SisterHead;
+	[SerializeField] float Speed = 0.3f;
+	[SerializeField] GameObject Eyes;
+	[SerializeField] float ViewDistance;
+	[SerializeField] GameObject PlayerHead;
+	[SerializeField] bool Rotate;
+	[SerializeField] Transform[] Waypoints;
+
+	private bool _sisterInSight;
+	private GameObject _player;                      
+	private RaycastHit _hit;
+	private Quaternion _startRot;
+	private int _cur = 0;
+	private Rigidbody _rigidbody;
+
 	
 	void Awake()
 	{
-		player = GameObject.FindWithTag("Player");
-		startRot = transform.rotation;
+		_player = GameObject.FindWithTag("Player");
+		_startRot = transform.rotation;
+		_rigidbody = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate()
 	{
-		if (playerInSight == false && sisterInSight == false)
+		if (PlayerInSight == false && _sisterInSight == false)
 		{
-		if (transform.position != waypoints [cur].position) 
+		if (transform.position != Waypoints [_cur].position) 
 		{
-			Vector3 p = Vector3.MoveTowards (transform.position, waypoints [cur].position, speed);
-			GetComponent<Rigidbody> ().MovePosition (p);
-			if (rot == true)
+			Vector3 p = Vector3.MoveTowards (transform.position, Waypoints [_cur].position, Speed);
+			_rigidbody.MovePosition (p);
+
+			if (Rotate == true)
 			{
 				transform.LookAt (p);
 			}
-		} 
-		if (transform.position == waypoints[cur].position)
+		}
+
+		if (transform.position == Waypoints[_cur].position)
 			{
-			cur = (cur + 1) % waypoints.Length;
+			_cur = (_cur + 1) % Waypoints.Length;
 			}
 		}
 	}
 
-		void OnTriggerStay (Collider other)
+	void OnTriggerStay (Collider other)
+	{
+
+		if(other.gameObject == _player)
 		{
+			Vector3 direction = PlayerHead.transform.position - Eyes.transform.position;
+			float angle = Vector3.Angle(direction, Eyes.transform.forward);
 
-			if(other.gameObject == player)
+			if(angle < FieldOfViewAngle * 0.5f)
 			{
-				Debug.Log("Is in Collider");
-				Vector3 direction = playerHead.transform.position - eyes.transform.position;
-				float angle = Vector3.Angle(direction, eyes.transform.forward);
 
-				if(angle < fieldOfViewAngle * 0.5f)
+				if(Physics.Raycast(Eyes.transform.position, direction.normalized, out _hit, ViewDistance))
 				{
-				Debug.DrawLine(eyes.transform.position, hit.point);
-					if(Physics.Raycast(eyes.transform.position, direction.normalized, out hit, viewDistance))
-					{
-						if(hit.collider.gameObject == player)
-						{
-							playerInSight = true;
 
-						if(sisterInSight == false)
+					if(_hit.collider.gameObject == _player)
+					{
+						PlayerInSight = true;
+
+						if(_sisterInSight == false)
 						{
-							transform.LookAt(playerHead.transform.position);
-						}
+							transform.LookAt(PlayerHead.transform.position);
 						}
 					}
 				}
 			}
+		}
 			
-		if (other.gameObject == sister)
+		if (other.gameObject == Sister)
 		{
-			Vector3 direction = sisterHead.transform.position - eyes.transform.position;
-			float angle = Vector3.Angle(direction, eyes.transform.forward);
+			Vector3 direction = SisterHead.transform.position - Eyes.transform.position;
+			float angle = Vector3.Angle(direction, Eyes.transform.forward);
 			
-			if(angle < fieldOfViewAngle * 0.5f)
+			if(angle < FieldOfViewAngle * 0.5f)
 			{
-				Debug.DrawLine(eyes.transform.position, hit.point);
-				if(Physics.Raycast(eyes.transform.position, direction.normalized, out hit, viewDistance))
+
+				if(Physics.Raycast(Eyes.transform.position, direction.normalized, out _hit, ViewDistance))
 				{
-					if(hit.collider.gameObject == sister)
+
+					if(_hit.collider.gameObject == Sister)
 					{
-						sisterInSight = true;
-						if (playerInSight == false)
+						_sisterInSight = true;
+
+						if (PlayerInSight == false)
 						{
-						transform.LookAt(sisterHead.transform.position);
+							transform.LookAt(SisterHead.transform.position);
 						}
 					}
 				}
@@ -97,16 +105,16 @@ public class SeePlayer : MonoBehaviour
 
 		void OnTriggerExit (Collider other)
 		{
-			if(other.gameObject == player)
+			if(other.gameObject == _player)
 			{
-				playerInSight = false;
-				transform.rotation = startRot;
+				PlayerInSight = false;
+				transform.rotation = _startRot;
 			}
 
-			if(other.gameObject == sister)
+			if(other.gameObject == Sister)
 			{
-				sisterInSight = false;
-				transform.rotation = startRot;
+				_sisterInSight = false;
+				transform.rotation = _startRot;
 			}
 		}
 }

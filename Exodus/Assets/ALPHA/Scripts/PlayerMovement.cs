962711b91ceb5e6eedffb6cbcfe7cb7426c43f34
unknown <Daniel Schneider@HNB07.gamer.dss.lan>
2015-12-05 10:49:15 +0100

    FINISHED City Level Building; FIXED PlayerMovement now relative to Camera;

--- Exodus/Assets/ALPHA/Scripts/PlayerMovement.cs
+++ Exodus/Assets/ALPHA/Scripts/PlayerMovement.cs
@@ -7,28 +7,30 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] float Speed;
 
     private NavMeshAgent _agent;
+	private GameObject _cam;
 
     void Start()
     {
-        _agent = GetComponent<NavMeshAgent>();
+		_agent = GetComponent<NavMeshAgent>();
     }	
 
-	void Update () 
+	void FixedUpdate () 
 	{
+		_cam = GameObject.Find("Camera");
+
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 
 		if (h != 0) 
 		{
-			transform.position += Vector3.right * Time.deltaTime * Speed *h;
-            transform.forward = Vector3.Normalize(new Vector3(h, 0f, v));
+			transform.position += _cam.transform.right * Time.deltaTime * Speed *h;
+            transform.forward = _cam.transform.TransformDirection(new Vector3(h, 0f, v));
         }
 
 		if (v != 0)
 		{
-			transform.position += Vector3.forward * Time.deltaTime * Speed *v;
-            transform.forward = Vector3.Normalize(new Vector3(h, 0f, v));
+			transform.position += _cam.transform.forward * Time.deltaTime * Speed *v;
+			transform.forward = _cam.transform.TransformDirection(new Vector3(h, 0f, v));
         }
-
 	}
 }

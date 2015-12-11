9ab9ce85272c959798dcde0e855fb4c5ebc423ad
unknown <Dan.Schneider@web.de>
2015-12-11 10:22:01 +0100

    ADDED Start Cutscene Script

---
+++ Exodus/Assets/Scripts/CutsceneSceneChange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneSceneChange : MonoBehaviour
+{
+    [SerializeField] float Interval;
+    [SerializeField] string SceneToLoad;
+
+	void Start ()
+    {
+        Invoke("LoadScene", Interval);
+	}
+
+    void LoadScene()
+    {
+        Application.LoadLevel(SceneToLoad);
+    }
+	
+}

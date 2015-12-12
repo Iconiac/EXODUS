9832341074552018f6ac0c1c3759492239ed8bb6
unknown <Nico Schnoor@HNB01.gamer.dss.lan>
2015-12-12 03:38:49 +0100

    CHANGED (Animator implemented as much as I can)

--- Exodus/Assets/Character/Character_AC_TestArea/Movement.cs
+++ Exodus/Assets/Character/Character_AC_TestArea/Movement.cs
@@ -3,55 +3,55 @@ using System.Collections;
 
 public class Movement : MonoBehaviour 
 {
-    public float speed = 6f;
+    [SerializeField] float Speed;
 
-    Vector3 movement;
+    private NavMeshAgent _agent;
+    private GameObject _cam;
+
     Animator anim;
-    Rigidbody playerRigidbody;
 
-    void Awake()
+    void Start()
     {
+        _agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
-        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        _cam = GameObject.Find("Camera");
 
-        Move(h, v);
-        Turning();
-        Animating(h, v);
-    }
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-    void Move(float h, float v)
-    {
-        movement.Set(h, 0f, v);
-
-        movement = movement.normalized * speed * Time.deltaTime;
-
-        playerRigidbody.MovePosition(transform.position + movement);
-    }
-
-    void Turning()
-    {
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit floorHit;
-
-        if (Physics.Raycast(camRay, out floorHit, camRayLenght, floorMask))
+        if (h != 0)
         {
-            Vector3 playerToMouse = floorHit.point - transform.position;
-            playerToMouse.y = 0f;
-
-            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-            playerRigidbody.MoveRotation(newRotation);
+            _agent.Move(_cam.transform.right * Time.deltaTime * Speed * h);
+            transform.forward = _cam.transform.TransformDirection(new Vector3(h, 0f, v));
         }
+
+        if (v != 0)
+        {
+            _agent.Move(_cam.transform.forward * Time.deltaTime * Speed * v);
+            transform.forward = _cam.transform.TransformDirection(new Vector3(h, 0f, v));
+        }
+
+        /*  if (Input.GetButton("Interact"))
+        {
+            anim.SetTrigger("Interact", interactOn);
+        }
+        */
+        Animating(h, v);
     }
 
     void Animating(float h, float v)
     {
         bool walking = h != 0f || v != 0f;
         anim.SetBool("IsWalking", walking);
+
+        if (Input.GetButton("Stealth"))
+        {
+            bool sneaking = h != 0f || v != 0f;
+            anim.SetBool("IsSneaking", sneaking);
+        }
     }
 }

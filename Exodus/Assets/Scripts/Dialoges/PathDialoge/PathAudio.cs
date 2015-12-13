846eb908287d24eadd5263e61f7d4cf3a97bd825
unknown <Dan.Schneider@web.de>
2015-12-13 22:03:28 +0100

    ADDED Voice Acting Path1 and Plattform

---
+++ Exodus/Assets/Scripts/Dialoges/PathDialoge/PathAudio.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathAudio : MonoBehaviour
+{
+    public AudioSource MalinaQuestion;
+    public AudioSource YesAnswer;
+    public AudioSource NoAnswer;
+    public AudioSource PortInfos;
+
+}
---
+++ Exodus/Assets/Scripts/Dialoges/PlattformDialoge/PlattformSounds.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlattformSounds : MonoBehaviour {
+
+    public AudioSource MalinaQuestion;
+    public AudioSource AaronAnswer;
+}
